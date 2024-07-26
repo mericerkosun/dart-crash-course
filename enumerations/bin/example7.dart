49112b1c66ff87141c2efbbea860653639737464
meric erkosun <meric.erkosun@gmail.com>
2024-07-26 10:00:31 +0300

    enumerations advance

---
+++ enumerations/bin/example7.dart
@@ -0,0 +1,22 @@
+// Fallthrough
+void main(List<String> args) {
+  final vehicle = VehicleType.car;
+
+  switch (vehicle) {
+    case VehicleType.motorcycle:
+    case VehicleType.bicycle:
+    case VehicleType.car:
+      print('Most common personal vehicles');
+      break;
+    case VehicleType.truck:
+      print('Usually used for work');
+      break;
+  }
+}
+
+enum VehicleType {
+  car,
+  truck,
+  motorcycle,
+  bicycle,
+}
